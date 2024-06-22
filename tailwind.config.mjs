92d48682753bf6672559f35ce466c615d3e46e65
aquapi <aquaplmc@gmail.com>
2024-06-22 17:15:33 +0700

    Testing

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,32 @@
 import daisyui from 'daisyui';
+import themes from 'daisyui/src/theming/themes';
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			georgian: ['Noto Serif Georgian Variable', 'serif'],
+			questrial: ['Questrial', 'san-serif']
+		},
+
 		extend: {},
 	},
 
 	plugins: [daisyui],
 	daisyui: {
-		themes: ['light', 'dark']
+		themes: [
+			{
+				light: {
+					...themes.light,
+
+					primary: '#2F2E2E',
+					neutral: '#2F2E2E',
+
+					'base-content': '#FFFFFF',
+					'neutral-content': 'rgb(179, 179, 179)'
+				}
+			}
+		],
 	}
 }
