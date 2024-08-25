a54d7efdc3239790e369623e58193c75940cb1f9
aquapi <aquaplmc@gmail.com>
2024-08-25 09:56:24 +0700

    Change once again

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
   prefetch: {
     defaultStrategy: 'load',
     prefetchAll: true
