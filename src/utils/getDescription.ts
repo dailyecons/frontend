d9670273c6eb1ad9ff4ba372c0ce708aaa695317
aquapi <aquaplmc@gmail.com>
2024-09-25 22:37:31 +0700

    I fix some displays

--- src/utils/getDescription.ts
+++ src/utils/getDescription.ts
@@ -1,3 +1,3 @@
 export default function getDescription(content: string, title: string) {
-  return content.split(' ').slice(0, title.length < 38 ? 30 : 20).join(' ') + '...';
+  return content.slice(0, title.length < 38 ? 160 : 135) + '...';
 }
