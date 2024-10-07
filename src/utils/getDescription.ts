8e1478b6b2ecd527489e776ab4e0fbca28ce8f0c
aquapi <aquaplmc@gmail.com>
2024-10-07 11:26:35 +0700

    Correctly slice title

--- src/utils/getDescription.ts
+++ src/utils/getDescription.ts
@@ -1,3 +1,3 @@
 export default function getDescription(content: string, title: string) {
-  return content.slice(0, title.length < 38 ? 160 : 135) + '...';
+  return content.slice(0, title.length < 38 ? 160 : title.length < 75 ? 135 : 92) + '...';
 }
